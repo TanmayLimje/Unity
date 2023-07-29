3d1744aa9cac3199a42b1359507020ec5773f818
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2023-07-29 20:39:18 +0530

    Mover.cs

---
+++ Mover.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{   
+    /*[SerializeField] float xValue = 0;
+    [SerializeField] float yValue = 0.84f;
+    [SerializeField] float zValue = 0;
+    */
+
+    [SerializeField] float moveSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue,0,zValue);
+    }
+}
